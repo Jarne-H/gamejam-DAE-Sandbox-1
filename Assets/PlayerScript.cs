05f5e731d19a1e1f9895665c7c860afaa19d5d7f
Jarne_H <horemans.jarne@gmail.com>
2023-11-04 13:52:21 +0100

    add: better movement + houses are the houses now

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class NpcScript : MonoBehaviour
@@ -6,10 +7,21 @@ public class NpcScript : MonoBehaviour
     public Rigidbody rb;
     public float velocity;
 
-    public float rotationSpeed = (float)1.2;
-    public float rotationAmount = 90;
+    public float rotationSpeed;
+
+    public float jumpheight;
+
+    public float previousPosY;
+    public float currentPosY;
 
     public bool mouseControl;
+    public Vector3 v = Vector3.zero;
+
+    private Vector3 forwards = Vector3.zero;
+    private Vector3 backwards = Vector3.zero;
+    private Vector3 left = Vector3.zero;
+    private Vector3 right = Vector3.zero;
+    private Vector3 jump = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -21,44 +33,52 @@ public class NpcScript : MonoBehaviour
 
     void Update()
     {
+        currentPosY = rb.position.y;
         if (mouseControl)
         {
             Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
+            Cursor.visible = false;
 
             mouseControl = false;
-        }
 
-        //movement
+        }
+        Vector3 v = Vector3.zero;
+
         if (Input.GetKey(KeyCode.W))
         {
-            rb.velocity = transform.forward * velocity;
+            v += transform.forward;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.velocity = transform.forward * -velocity;
+            v -= transform.forward;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.velocity = transform.right * -velocity;
+            v -= transform.right;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.velocity = transform.right * velocity;
+            v += transform.right;
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        //if (Input.GetKeyDown(KeyCode.Space) && currentPosY == previousPosY && currentPosY < 2.5f)
+        //{
+        //    v += transform.up*jumpheight;
+        //}
+
+        if (v != Vector3.zero)
         {
-            rb.velocity = Vector3.up;
+            v.Normalize();
         }
-
-
+        rb.AddForce(v * velocity);
 
         //rotation
-        if (Input.mousePosition.x > Screen.width / 2 +10 || Input.mousePosition.x < Screen.width / 2 - 10)
+        if (Input.mousePosition.x > Screen.width / 2 + 10 || Input.mousePosition.x < Screen.width / 2 - 10)
         {
-            transform.Rotate(0, (Input.mousePosition.x - Screen.width/2) * rotationSpeed * Time.deltaTime, 0);
+            transform.Rotate(0, ((Input.mousePosition.x - Screen.width / 2) * rotationSpeed) * Time.deltaTime, 0);
             resetMousePos();
         }
+        previousPosY = currentPosY;
     }
 
     private void resetMousePos()
@@ -68,5 +88,6 @@ public class NpcScript : MonoBehaviour
 
         mouseControl = true;
     }
+
 
 }
